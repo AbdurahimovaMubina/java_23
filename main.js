841d8d8fa0f3fb6854e5d679f25afc8ec063992d
AbdurahimovaMubina <mubinaabdurahimova87@gmail.com>
2024-12-08 15:38:55 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,60 @@
+// const BASE_URL = 'https://api.nationalize.io/'
+// let params = '?name='
+
+// let ism = 'bahodir'
+
+// let FULL_API = BASE_URL +params+ ism
+
+// const getData = async () => {
+//     const res = await fetch(FULL_API)
+//     const data = await res.json()
+
+//     console.log(data);   
+// }
+// getData()
+
+
+
+
+
+
+
+
+const __API = "https://randomuser.me/api/";
+
+const btn = document.querySelector("button");
+const getData = async () => {
+  const res = await fetch(__API);
+  const data = await res.json();
+
+  let obj = data.results[0];
+  render(obj);
+};
+
+getData();
+
+function render(obj) {
+  console.log(obj);
+
+  let img = document.querySelector(".imgg");
+  img.src = `${obj.picture.large}`;
+
+  let text = document.querySelector(".text");
+  text.textContent = `${obj.name.title} ${obj.name.first} ${obj.name.last}`;
+
+  let text2 = document.querySelector(".text2");
+  text2.textContent = `Phone: ${obj.cell}`;
+
+  let text3 = document.querySelector(".text3");
+  text3.textContent = `Manzil: ${obj.location.state}`;
+
+  let text4 = document.querySelector(".text4");
+  text4.textContent = `Yosh: ${obj.dob.age}`;
+
+  let text5 = document.querySelector(".text5");
+  text5.textContent = `Email: ${obj.email}`;
+}
+
+btn.addEventListener("click", () => {
+  getData();
+});
